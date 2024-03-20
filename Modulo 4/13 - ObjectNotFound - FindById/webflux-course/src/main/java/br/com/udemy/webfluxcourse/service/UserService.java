3529937a050a44f552371da995d5cdb1464e037e
Rafael <rafaeldealmeida.192001@gmail.com>
2024-03-20 02:50:16 -0300

    add exception handler to object not found

--- Modulo 4/13 - ObjectNotFound - FindById/webflux-course/src/main/java/br/com/udemy/webfluxcourse/service/UserService.java
+++ Modulo 4/13 - ObjectNotFound - FindById/webflux-course/src/main/java/br/com/udemy/webfluxcourse/service/UserService.java
@@ -4,6 +4,7 @@ import br.com.udemy.webfluxcourse.entity.User;
 import br.com.udemy.webfluxcourse.mapper.UserMapper;
 import br.com.udemy.webfluxcourse.model.request.UserRequest;
 import br.com.udemy.webfluxcourse.repository.UserRepository;
+import br.com.udemy.webfluxcourse.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -20,6 +21,10 @@ public class UserService {
     }
 
     public Mono<User> findById(final String id) {
-        return repository.findById(id);
+        return repository.findById(id).switchIfEmpty(
+                Mono.error(new ObjectNotFoundException(
+                        String.format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName())
+                ))
+        );
     }
 }
