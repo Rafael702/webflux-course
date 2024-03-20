3d081749396ada2259e54c87af66a4c575c9cc67
Rafael <rafaeldealmeida.192001@gmail.com>
2024-03-20 02:02:40 -0300

    Validacao para strings

---
+++ Modulo 4/11 - Validar Strings/webflux-course/src/main/java/br/com/udemy/webfluxcourse/controller/UserController.java
@@ -0,0 +1,33 @@
+package br.com.udemy.webfluxcourse.controller;
+
+import br.com.udemy.webfluxcourse.model.request.UserRequest;
+import br.com.udemy.webfluxcourse.model.response.UserResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+
+    @PostMapping
+    ResponseEntity<Mono<Void>> save(@Valid @RequestBody final UserRequest request);
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> find(@PathVariable String id);
+
+    @GetMapping
+    ResponseEntity<Flux<UserResponse>> findAll();
+
+    @PatchMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
+
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Mono<Void>> delete(@PathVariable String id);
+
+}
