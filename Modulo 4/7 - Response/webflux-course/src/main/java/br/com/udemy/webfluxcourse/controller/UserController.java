29b5d5954ec5cf08b9615283373a66bff0537f39
Rafael <rafaeldealmeida.192001@gmail.com>
2024-03-20 00:15:18 -0300

    Add imports of the dto's

--- Modulo 4/7 - Response/webflux-course/src/main/java/br/com/udemy/webfluxcourse/controller/UserController.java
+++ Modulo 4/7 - Response/webflux-course/src/main/java/br/com/udemy/webfluxcourse/controller/UserController.java
@@ -1,11 +1,14 @@
 package br.com.udemy.webfluxcourse.controller;
 
+import br.com.udemy.webfluxcourse.model.request.UserRequest;
+import br.com.udemy.webfluxcourse.model.response.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserController {
