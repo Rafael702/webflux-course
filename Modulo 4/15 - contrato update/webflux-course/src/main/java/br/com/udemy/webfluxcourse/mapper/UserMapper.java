65cc894656a92302dc003d3198e01f6e49c62475
Rafael <rafaeldealmeida.192001@gmail.com>
2024-03-20 03:17:59 -0300

    add update user

--- Modulo 4/15 - contrato update/webflux-course/src/main/java/br/com/udemy/webfluxcourse/mapper/UserMapper.java
+++ Modulo 4/15 - contrato update/webflux-course/src/main/java/br/com/udemy/webfluxcourse/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import br.com.udemy.webfluxcourse.model.request.UserRequest;
 import br.com.udemy.webfluxcourse.model.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -17,6 +18,9 @@ public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request, @MappingTarget final User entity);
 
     UserResponse toResponse(final User entity);
 }
