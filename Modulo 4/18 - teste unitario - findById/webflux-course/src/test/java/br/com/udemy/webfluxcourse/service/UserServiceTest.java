01e5bf2edfee77ccaa6e9ff595be7f4327726037
Rafael <rafaeldealmeida.192001@gmail.com>
2024-03-21 00:13:42 -0300

    Start test to find By Id

---
+++ Modulo 4/18 - teste unitario - findById/webflux-course/src/test/java/br/com/udemy/webfluxcourse/service/UserServiceTest.java
@@ -0,0 +1,56 @@
+package br.com.udemy.webfluxcourse.service;
+
+import br.com.udemy.webfluxcourse.entity.User;
+import br.com.udemy.webfluxcourse.mapper.UserMapper;
+import br.com.udemy.webfluxcourse.model.request.UserRequest;
+import br.com.udemy.webfluxcourse.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    void testSave() {
+        UserRequest request = new UserRequest("rafael", "rafael@mail.com", "123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void testFindById() {
+        
+    }
+}
